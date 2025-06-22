88ce579f2cf4621ba00e84841d6006c9c78da912
Wadha Nayef Alsheddi <134851146+wsheddi@users.noreply.github.com>
2025-06-22 18:45:57 +0300

    Add files via upload

--- FlaskWebProject/__init__.py
+++ FlaskWebProject/__init__.py
@@ -10,7 +10,24 @@ from flask_session import Session
 
 app = Flask(__name__)
 app.config.from_object(Config)
-# TODO: Add any logging levels and handlers with app.logger
+import os
+
+if not app.debug:
+    # Create a file handler
+    if not os.path.exists('logs'):
+        os.mkdir('logs')
+
+    file_handler = logging.FileHandler('logs/app.log')
+    file_handler.setLevel(logging.INFO)
+
+    # Set a simple log format
+    formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+    file_handler.setFormatter(formatter)
+
+    # Add the handler to the app's logger
+    app.logger.addHandler(file_handler)
+    app.logger.setLevel(logging.INFO)
+    app.logger.info('App startup')
 Session(app)
 db = SQLAlchemy(app)
 login = LoginManager(app)
