dab242235fa0f81336c6473e5b8a7c5245c10aac
Wadha Nayef Alsheddi <134851146+wsheddi@users.noreply.github.com>
2025-06-23 11:27:45 +0300

    Update config.py

--- config.py
+++ config.py
@@ -3,30 +3,33 @@ import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'ksdfj0239kjsdlfji!@#89234'
+    # Secret key for Flask sessions
+    SECRET_KEY = os.environ.get('SECRET_KEY', 'ksdfj0239kjsdlfji!@#89234')
+    
+    # Azure Blob Storage
+    BLOB_ACCOUNT = os.environ.get('BLOB_ACCOUNT', 'cmssstoragewadha')
+    BLOB_CONTAINER = os.environ.get('BLOB_CONTAINER', 'images')
+    BLOB_STORAGE_KEY = os.environ.get('BLOB_STORAGE_KEY', '1Yq/2EqrzvtcyfSnuHLew6WiVTk7qHIvKBxHPVJ0EIe16JCBqFRwfifNI+MyRVqUF3/fZqIwvH63+AStM//KLw==')
 
-    BLOB_ACCOUNT = os.environ.get('BLOB_ACCOUNT') or 'cmssstoragewadha'
-    BLOB_STORAGE_KEY = os.environ.get('BLOB_STORAGE_KEY') or '1Yq/2EqrzvtcyfSnuHLew6WiVTk7qHIvKBxHPVJ0EIe16JCBqFRwfifNI+MyRVqUF3/fZqIwvH63+AStM//KLw=='
-    BLOB_CONTAINER = os.environ.get('BLOB_CONTAINER') or 'images'
-
-    SQL_SERVER = os.environ.get('SQL_SERVER') or 'cmsdbserverwadha.database.windows.net'
-    SQL_DATABASE = os.environ.get('SQL_DATABASE') or 'cmsdb'
-    SQL_USER_NAME = os.environ.get('SQL_USER_NAME') or 'Admin1'
-    SQL_PASSWORD = os.environ.get('SQL_PASSWORD') or 'Wadha@10'
-
-    # Important: Make sure this line is properly indented inside the class!
+    # SQL Database connection
+    SQL_SERVER = os.environ.get('SQL_SERVER', 'cmsdbserverwadha.database.windows.net')
+    SQL_DATABASE = os.environ.get('SQL_DATABASE', 'cmsdb')
+    SQL_USER_NAME = os.environ.get('SQL_USER_NAME', 'Admin1')
+    SQL_PASSWORD = os.environ.get('SQL_PASSWORD', 'Wadha@10')
     SQLALCHEMY_DATABASE_URI = (
-        "mssql+pyodbc://Admin1:Wadha%4010@cmsdbserverwadha.database.windows.net:1433/cmsdb"
-        "?driver=ODBC+Driver+17+for+SQL+Server&Encrypt=yes"
+        f"mssql+pyodbc://{SQL_USER_NAME}:{SQL_PASSWORD}@{SQL_SERVER}:1433/"
+        f"{SQL_DATABASE}?driver=ODBC+Driver+17+for+SQL+Server&Encrypt=yes"
     )
-
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
-    # MSAL (Microsoft Authentication) Info
-    CLIENT_SECRET = os.environ.get('CLIENT_SECRET') or "bEZ8Q~mn9ueWDVxZMoBPpHsM7pcmsUwVh~QTPdbN"
-    CLIENT_ID = os.environ.get('CLIENT_ID') or "9d2b1629-415e-460e-b635-eda4dbd1d0c9"
-    AUTHORITY = os.environ.get('AUTHORITY') or "https://login.microsoftonline.com/2c86bbfc-8d04-41ff-a83a-942f075e0f60"
-    REDIRECT_PATH = os.environ.get('REDIRECT_PATH') or "/getAToken"  # Must match your app registration
+    # MS Identity Platform (Azure AD)
+    CLIENT_ID = os.environ.get('CLIENT_ID', '9d2b1629-415e-460e-b635-eda4dbd1d0c9')
+    CLIENT_SECRET = os.environ.get('CLIENT_SECRET', 'bEZ8Q~mn9ueWDVxZMoBPpHsM7pcmsUwVh~QTPdbN')
+    REDIRECT_PATH = os.environ.get('REDIRECT_PATH', '/getAToken')
+    AUTHORITY = os.environ.get('AUTHORITY', 'https://login.microsoftonline.com/2c86bbfc-8d04-41ff-a83a-942f075e0f60')
 
+    # MS Graph scope
     SCOPE = ["User.Read"]
-    SESSION_TYPE = "filesystem"  # Token cache will be stored in server-side session
+
+    # Flask session type
+    SESSION_TYPE = "filesystem"
